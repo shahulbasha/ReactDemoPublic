f18911529562eb593dfcae608c64a5bb87f87bfb
ShahulBasha <shhl.basha333@gmail.com>
2019-05-18 23:28:52 +0530

    Error Boundary Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import CardList from "./CardList";
 import axios from "axios";
 import SearchBox from "./SearchBox";
+import ErrorBoundary from "./ErrorBoundary";
 import "./App.css";
 
 class App extends React.Component {
@@ -25,25 +26,24 @@ class App extends React.Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.username
-        .toLowerCase()
-        .includes(this.state.searchField.toLowerCase());
+    const { robots, searchField } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.username.toLowerCase().includes(searchField.toLowerCase());
     });
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div>
-          <h1>
-            <span className="logo1">R</span>OBOFRIENDS
-          </h1>
-          <SearchBox searchChange={this.onSearchChange} />
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div>
+        <h1>
+          <span className="logo1">R</span>OBOFRIENDS
+        </h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <ErrorBoundary>
           <CardList robots={filteredRobots} />
-        </div>
-      );
-    }
+        </ErrorBoundary>
+      </div>
+    );
   }
 }
 
